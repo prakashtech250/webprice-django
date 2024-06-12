b2805b3f20b5c280c64351dd77e60ac988cd0978
Prakash <prakashtech250@gmail.com>
2024-06-12 10:42:39 +0545

    jun12

--- webprice/settings.py
+++ webprice/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-k59g*za)_)q*z+8c#mwk^mpentj3v9te=5ws@4k))ht(_$i#_=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
 
 
 # Application definition
