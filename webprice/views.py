1348e6f42234bfbc7a2e5b403970310a6e6a93a6
Prakash <prakashtech250@gmail.com>
2024-06-12 13:41:02 +0545

    jun 12

--- webprice/views.py
+++ webprice/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
 
 def home(request):
-    if request.user.is_authenticated:
-        return redirect('dashboard')
-    else:
-        return redirect('login')
-    # return render(request, 'index.html')
+    # if request.user.is_authenticated:
+    #     return redirect('dashboard')
+    # else:
+    #     return redirect('login')
+    return render(request, 'index.html')
